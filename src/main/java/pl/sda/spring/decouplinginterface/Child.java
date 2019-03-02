2a00c5193ccc4a04c460a2a7d8b4329775f873e3
Marcin Iskra <dunravel@gmail.com>
2019-03-02 13:59:24 +0100

    Added @Component to Child

--- src/main/java/pl/sda/spring/decouplinginterface/Child.java
+++ src/main/java/pl/sda/spring/decouplinginterface/Child.java
@@ -1,5 +1,8 @@
 package pl.sda.spring.decouplinginterface;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class Child {
     public String getName(){
         return "My name is...";
